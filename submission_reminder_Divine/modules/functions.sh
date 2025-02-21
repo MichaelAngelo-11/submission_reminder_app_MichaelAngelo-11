54b2c5250343b0070d3326b7565527ba4988b9c5
root <p.ruackibun@alustudent.com>
2025-02-21 11:42:50 -0800

    final

--- submission_reminder_Divine/modules/functions.sh
+++ submission_reminder_Divine/modules/functions.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
 function check_submissions {
     local file=$1
     echo "Checking submissions in $file"
-    while IFS=, read -r student assignment status; do
+    while IFS=',' read -r student assignment status; do
+        # Remove leading and trailing spaces
+        student=$(echo $student | xargs)
+        assignment=$(echo $assignment | xargs)
+        status=$(echo $status | xargs)
+
         if [[ "$assignment" == "$ASSIGNMENT" && "$status" == "not submitted" ]]; then
             echo "$student has not submitted $ASSIGNMENT!"
         fi
